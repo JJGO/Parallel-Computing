12f89af5468198510194f60e2a2cd7f531e40559
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2015-11-01 20:25:46 -0500

    Init Homework 4
    Serial Implementation of Homework 4

---
+++ 4 Homework 4/0 Serial/Serial.py
@@ -0,0 +1,42 @@
+C = 13
+coords = [ ( 1.1*(i**2 %17) , 0.5*(i**3 %23) ) for i in range(C)]
+
+distance = {}
+for i in range(C):
+	distance[(i,i)] = 0
+	p1,p2 = coords[i]
+	for j in range(i):
+		q1,q2 = coords[j]
+		distance[(i,j)] = (abs(p1-q1)**1.5+abs(p2-q2)**1.5)**(2.0/3.0)
+		distance[(j,i)] = distance[(i,j)]
+
+paths = [(0.0,[0])]
+bound = ([],"_")
+while paths:
+	dist, path = paths.pop()
+	last = path[-1]
+	unvisited = [x for x in range(C) if x not in path]
+	if unvisited:
+		for node in unvisited :
+			newdist = dist + distance[(last,node)]
+			if newdist <= bound[0]:
+				paths.append((newdist,path+[node]))
+	else:
+		if dist < bound[0]:
+			bound = (dist,path)
+
+
+print coords
+print
+
+for i in range(C):
+	for j in range(C):
+		print "{0:0.2f}".format(distance[(i,j)]).zfill(5),
+	print
+
+print bound
+best = bound[1]
+total = 0.0
+for i in range(len(best)-1):
+	total += distance[(best[i],best[i+1])]
+print total
---
+++ 4 Homework 4/0 Serial/serial.cpp
@@ -0,0 +1,170 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+// #include <iostream>
+#include <cmath>
+#include <time.h>
+#include <limits>
+#include <vector>
+#include <stack>
+#include <queue>
+
+#define C 14
+
+// using namespace std;
+
+struct Point {
+    double x;
+    double y;
+};
+// typedef struct Point Point;
+
+struct Path
+{
+    std::vector<int> order;
+    bool visited[C];
+    double cost;
+} ;
+
+// struct ComparePaths
+// {
+//     bool operator()(const Path &l, const Path &r) {return l.cost/l.order.size() > r.cost/r.order.size();}
+// };
+
+struct ComparePaths // LessThan
+{
+    bool operator()(const Path &l, const Path &r) {
+        if(l.order.size() == r.order.size())
+            return l.cost > r.cost;
+        else
+            return l.order.size() < r.order.size();
+    }
+};
+
+void initialize_coordinates(Point coordinates[C]);
+void calculate_distances(Point coordinates[C], double distances[C][C]);
+void print_matrix(double A[C][C]);
+void print_path(Path path);
+
+
+int main(int argc, char const *argv[])
+{
+    Point coordinates[C];
+    double distances[C][C];
+
+    // std::stack<Path> unexplored_paths;
+    std::priority_queue<Path,std::vector<Path>,ComparePaths> unexplored_paths;
+
+    Path best_path;
+    best_path.cost = std::numeric_limits<float>::infinity();
+
+    Path origin, current_path, new_path;
+    origin.cost = 0.0;
+    origin.order.push_back(0);
+    for(int i = 0; i < C; i++)
+    {
+        origin.visited[i] = false;
+    }
+
+    clock_t begin, end;
+    double time_spent;
+
+    begin = clock();
+
+    initialize_coordinates(coordinates);
+    calculate_distances(coordinates,distances);
+
+    unexplored_paths.push(origin);
+
+    while(!unexplored_paths.empty())
+    {
+        current_path = unexplored_paths.top();
+        unexplored_paths.pop();
+        for(int node = 1 ; node < C ; node++)
+        {
+            new_path = current_path;
+            if(!new_path.visited[node])
+            {
+                new_path.cost += distances[new_path.order.back()][node];
+                if(new_path.cost < best_path.cost)
+                {
+                    new_path.visited[node] = true;
+                    new_path.order.push_back(node);
+                    if(new_path.order.size() < C)
+                    {
+                        unexplored_paths.push(new_path);
+                    }
+                    else
+                    {
+                        best_path = new_path;
+                    }
+                }
+                // print_path(new_path);
+            }
+        }
+    }
+
+    // printf("\n SUM : %lf\n", sumA );
+    // printf("\n MIN : %lf\n", minA );
+
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    print_matrix(distances);
+    print_path(best_path);
+    printf("\nTIME : %lf seconds\n", time_spent);
+
+    return 0;
+
+}
+
+
+void initialize_coordinates(Point coordinates[C])
+{
+    for (int c = 0; c < C; c++)
+    {
+        coordinates[c].x = 1.1*(c*c % 17);
+        coordinates[c].y = 0.5*(c*c*c % 23);
+    }
+}
+
+void calculate_distances(Point coordinates[C], double distances[C][C])
+{
+    int i,j;
+    double dist, deltaX, deltaY, iX, iY;
+    for( i = 0; i < C; i++)
+    {
+        iX = coordinates[i].x;
+        iY = coordinates[i].y;
+        distances[i][i] = 0.0;
+        for(j = 0; j < i; j++)
+        {
+            deltaX = pow(abs(iX-coordinates[j].x),1.5);
+            deltaY = pow(abs(iY-coordinates[j].y),1.5);
+            dist = pow(deltaX+deltaY,2.0/3.0);
+            distances[i][j] = dist;
+            distances[j][i] = dist;
+        }
+    }
+}
+
+void print_matrix(double A[C][C])
+{
+    for (int i = 0; i < C; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            printf("%lf ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_path(Path path)
+{
+    for(int i = 0 ; i < path.order.size() ; i++)
+    {
+        printf("%d, ", path.order[i]);
+    }
+    printf("\b\b : %lf\n",path.cost);
+}
+
